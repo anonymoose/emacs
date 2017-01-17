2c3cbec8ec3dd90c7bd9f2d48d021e4e27aba9d1
anonymoose <ken.bedwell@randrr.com>
2017-01-17 13:37:25 -0500

    project mode config

--- init.el
+++ init.el
@@ -18,9 +18,20 @@
 ;(add-hook 'cider-repl-mode-hook #'company-mode)
 ;(add-hook 'cider-mode-hook #'company-mode)
 
+;; configure melpa as one of our repos
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+
+
 ; https://github.com/pashky/restclient.el
 (live-add-pack-lib "restclient.el")
 (require 'restclient)
+
+
+
+;(require 'project-explorer)
+(global-set-key (kbd "M-e") 'project-explorer-toggle)
+
 
 (add-to-list 'load-path "~/.live-packs/ken-pack/lib/csv-mode")
 (load "csv-mode")
